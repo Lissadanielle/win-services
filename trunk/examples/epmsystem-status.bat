547b7128e332949eec40aff1017f073af16a9e95
tomas.martinez.soldevilla <tomas.martinez.soldevilla@468fc67a-d520-2231-42f7-c7aded8794f4>
2010-11-05 11:25:30 +0000

    First Version (from RCS)

---
+++ trunk/examples/epmsystem-status.bat
@@ -0,0 +1,2 @@
+winservices.exe -c -l epmsystem.start -a STATUS -d F:\Oracle\admin\reports 
+pause
---
+++ trunk/utils/verbose_inc.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+print "This is sample.pl\n";
+print "ARGV = ", join(" ", @ARGV), "\n";
+
+print "Script path \$0 = $0\n";
+print "Exe path \$^X = $^X\n";
+print "Perl verison \$] = $]\n";
+
+print "\@INC=\n", join("\n", @INC), "\n";
+sleep (1);
